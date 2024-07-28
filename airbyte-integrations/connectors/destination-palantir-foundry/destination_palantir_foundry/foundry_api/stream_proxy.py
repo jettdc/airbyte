75e6b4769214039f204faf9ff930931958a28452
Jett Crowson <jcrowson@palantir.com>
2024-07-28 16:08:28 -0600

    logging

--- airbyte-integrations/connectors/destination-palantir-foundry/destination_palantir_foundry/foundry_api/stream_proxy.py
+++ airbyte-integrations/connectors/destination-palantir-foundry/destination_palantir_foundry/foundry_api/stream_proxy.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict, List, Any
 
 from foundry._core.auth_utils import Auth
@@ -36,6 +37,9 @@ class GetRecordsResponse(BaseModel):
     records: List[RecordV2]
 
 
+logger = logging.getLogger("airbyte")
+
+
 class StreamProxy(FoundryService):
     def __init__(self, foundry_host: str, api_auth: Auth) -> None:
         self.api_client = FoundryApiClient(
@@ -58,6 +62,10 @@ class StreamProxy(FoundryService):
             body=PutJsonRecordsRequest([PutJsonRecordRequest(value=record) for record in records]),
             request_timeout=REQUEST_TIMEOUT,
         )
+
+        # TODO(jcrowson): Remove!
+        logger.info("RECORDS######")
+        logger.info(records)
 
         return self.api_client.call_api(put_json_record_request)
 
