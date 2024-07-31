e79d088dec1263f2cbd00c9aaf2994a7a36156aa
Jett Crowson <jcrowson@palantir.com>
2024-07-30 18:45:35 -0600

    ignore unknown fields

--- airbyte-integrations/connectors/destination-palantir-foundry/destination_palantir_foundry/foundry_schema/converters/airbyte_field_converter.py
+++ airbyte-integrations/connectors/destination-palantir-foundry/destination_palantir_foundry/foundry_schema/converters/airbyte_field_converter.py
@@ -33,12 +33,12 @@ All types can be nullable.
 IS_JSON = {"is_json": True}
 
 
-def _find_field_schema(field_name: str, field_schemas: List[FoundryFieldSchema]) -> FoundryFieldSchema:
+def _find_field_schema(field_name: str, field_schemas: List[FoundryFieldSchema]) -> Optional[FoundryFieldSchema]:
     for field_schema in field_schemas:
         if field_schema.name == field_name:
             return field_schema
 
-    raise ValueError(f"Could not find field schema for field {field_name} in Foundry schema.")
+    return None
 
 
 def _convert_timestamp_to_unix_ms(timestamp: str) -> float:
@@ -65,11 +65,13 @@ class FoundryFieldDescription(NamedTuple):
     custom_metadata: Optional[Dict[str, Any]] = None
 
 
-DEFAULT_FOUNDRY_FIELD_DESCRIPTION = FoundryFieldDescription(field_schema=StringFieldSchema, custom_metadata=IS_JSON)
+DEFAULT_FOUNDRY_FIELD_DESCRIPTION = FoundryFieldDescription(
+    field_schema=StringFieldSchema, custom_metadata=IS_JSON)
 
 
 def _get_ab_type_from_arr(types: List[str]) -> Optional[str]:
-    types_without_null = list(filter(lambda field_type: field_type != "null", types))
+    types_without_null = list(
+        filter(lambda field_type: field_type != "null", types))
     return types_without_null[0] if len(types_without_null) > 0 else None
 
 
@@ -77,63 +79,66 @@ class AirbyteToFoundryConverter:
     type_mappings: Dict[AirbyteFieldDescription, FoundryFieldDescription] = {
         # String
         AirbyteFieldDescription(type_name="string"): \
-            FoundryFieldDescription(field_schema=StringFieldSchema),
+        FoundryFieldDescription(field_schema=StringFieldSchema),
 
         # Boolean
         AirbyteFieldDescription(type_name="boolean"): \
-            FoundryFieldDescription(field_schema=BooleanFieldSchema),
+        FoundryFieldDescription(field_schema=BooleanFieldSchema),
 
         # Date
         AirbyteFieldDescription(type_name="string", format="date"): \
-            FoundryFieldDescription(field_schema=DateFieldSchema),
+        FoundryFieldDescription(field_schema=DateFieldSchema),
 
         # Timestamp
         AirbyteFieldDescription(type_name="string", format="date-time", airbyte_type="timestamp_without_timezone"): \
-            FoundryFieldDescription(field_schema=TimestampFieldSchema),
+        FoundryFieldDescription(field_schema=TimestampFieldSchema),
         AirbyteFieldDescription(type_name="string", format="date-time", airbyte_type="timestamp_with_timezone"): \
-            FoundryFieldDescription(field_schema=TimestampFieldSchema),
+        FoundryFieldDescription(field_schema=TimestampFieldSchema),
         AirbyteFieldDescription(type_name="string", format="date-time"): \
-            FoundryFieldDescription(field_schema=TimestampFieldSchema),
+        FoundryFieldDescription(field_schema=TimestampFieldSchema),
 
         # Time
         AirbyteFieldDescription(type_name="string", format="time", airbyte_type="time_without_timezone"): \
-            FoundryFieldDescription(field_schema=StringFieldSchema),
+        FoundryFieldDescription(field_schema=StringFieldSchema),
         AirbyteFieldDescription(type_name="string", format="time", airbyte_type="time_with_timezone"): \
-            FoundryFieldDescription(field_schema=StringFieldSchema),
+        FoundryFieldDescription(field_schema=StringFieldSchema),
 
         # Integer
         AirbyteFieldDescription(type_name="integer"): \
-            FoundryFieldDescription(field_schema=LongFieldSchema),
+        FoundryFieldDescription(field_schema=LongFieldSchema),
         AirbyteFieldDescription(type_name="number", airbyte_type="integer"): \
-            FoundryFieldDescription(field_schema=LongFieldSchema),
+        FoundryFieldDescription(field_schema=LongFieldSchema),
 
         # Number
         AirbyteFieldDescription(type_name="number"): \
-            FoundryFieldDescription(field_schema=DoubleFieldSchema),
+        FoundryFieldDescription(field_schema=DoubleFieldSchema),
 
         # Array
         AirbyteFieldDescription(type_name="array"): \
-            FoundryFieldDescription(field_schema=ArrayFieldSchema),
+        FoundryFieldDescription(field_schema=ArrayFieldSchema),
         AirbyteFieldDescription(type_name="array", arr_is_union=False): \
-            FoundryFieldDescription(field_schema=ArrayFieldSchema),
+        FoundryFieldDescription(field_schema=ArrayFieldSchema),
         AirbyteFieldDescription(type_name="array", arr_is_union=True): \
-            FoundryFieldDescription(field_schema=ArrayFieldSchema),
+        FoundryFieldDescription(field_schema=ArrayFieldSchema),
 
         # Object
         AirbyteFieldDescription(type_name="object"): \
-            FoundryFieldDescription(field_schema=StructFieldSchema),
+        FoundryFieldDescription(field_schema=StructFieldSchema),
 
         # Union
         AirbyteFieldDescription(type_name="object", obj_is_union=True): \
-            FoundryFieldDescription(field_schema=StringFieldSchema, custom_metadata=IS_JSON),
+        FoundryFieldDescription(
+            field_schema=StringFieldSchema, custom_metadata=IS_JSON),
         AirbyteFieldDescription(obj_is_union=True): \
-            FoundryFieldDescription(field_schema=StringFieldSchema, custom_metadata=IS_JSON),
+        FoundryFieldDescription(
+            field_schema=StringFieldSchema, custom_metadata=IS_JSON),
     }
 
     @staticmethod
     def convert_ab_to_foundry_field_schema(ab_field_name: Optional[str], ab_field_schema: Dict[str, Any]) -> FoundryFieldSchema:
         # Can be a single type or [type, "null"]
-        ab_field_type: Union[str, List[str], None] = ab_field_schema.get("type", None)
+        ab_field_type: Union[str, List[str],
+                             None] = ab_field_schema.get("type", None)
 
         if ab_field_type is None:
             return StringFieldSchema(
@@ -161,9 +166,11 @@ class AirbyteToFoundryConverter:
         airbyte_type = ab_field_schema.get("airbyte_type", None)
 
         items_type = ab_field_schema.get("items", None)
-        arr_is_union = None if items_type is None else isinstance(items_type, list)
+        arr_is_union = None if items_type is None else isinstance(
+            items_type, list)
 
-        obj_is_union = None if ab_field_schema.get("oneOf", None) is None else True
+        obj_is_union = None if ab_field_schema.get(
+            "oneOf", None) is None else True
 
         foundry_field_description = AirbyteToFoundryConverter.type_mappings.get(AirbyteFieldDescription(
             type_name=parsed_type,
@@ -184,7 +191,8 @@ class AirbyteToFoundryConverter:
         if foundry_field_description.field_schema == ArrayFieldSchema:
             item_type: Optional[Dict] = ab_field_schema.get("items", None)
             if item_type is not None and not isinstance(items_type, list):
-                foundry_field_schema_kwargs["arraySubtype"] = AirbyteToFoundryConverter.convert_ab_to_foundry_field_schema(None, item_type)
+                foundry_field_schema_kwargs["arraySubtype"] = AirbyteToFoundryConverter.convert_ab_to_foundry_field_schema(
+                    None, item_type)
             else:
                 foundry_field_schema_kwargs["arraySubtype"] = StringFieldSchema(
                     nullable=True,
@@ -195,7 +203,8 @@ class AirbyteToFoundryConverter:
             # if additional props, probably make map
             sub_schemas: List[FoundryFieldSchema] = []
             for obj_property_name, obj_property_schema in ab_field_schema.get("properties", {}).items():
-                sub_schemas.append(AirbyteToFoundryConverter.convert_ab_to_foundry_field_schema(obj_property_name, obj_property_schema))
+                sub_schemas.append(AirbyteToFoundryConverter.convert_ab_to_foundry_field_schema(
+                    obj_property_name, obj_property_schema))
 
             if len(sub_schemas) == 0:
                 return StringFieldSchema(
@@ -208,8 +217,10 @@ class AirbyteToFoundryConverter:
 
         ab_description = ab_field_schema.get("description", None)
         if ab_description is not None:
-            custom_metadata = foundry_field_schema_kwargs.get("customMetadata", {})
-            foundry_field_schema_kwargs["customMetadata"] = {**custom_metadata, "ab_description": ab_description}
+            custom_metadata = foundry_field_schema_kwargs.get(
+                "customMetadata", {})
+            foundry_field_schema_kwargs["customMetadata"] = {
+                **custom_metadata, "ab_description": ab_description}
 
         return foundry_field_description.field_schema(**foundry_field_schema_kwargs)
 
@@ -221,26 +232,31 @@ class AirbyteToFoundryConverter:
         if field_schema.customMetadata.get("is_json", False):
             return json.dumps(value)
         if value is None and not field_schema.nullable:
-            raise ValueError(f"Cannot parse null value for nonnullable field: {field_schema.name}")
+            raise ValueError(
+                f"Cannot parse null value for nonnullable field: {field_schema.name}")
 
         if isinstance(field_schema, ArrayFieldSchema):
             if not isinstance(value, list):
-                raise ValueError(f"Cannot parse nonlist array type: {field_schema.name}")
+                raise ValueError(
+                    f"Cannot parse nonlist array type: {field_schema.name}")
 
             return [AirbyteToFoundryConverter.convert_record_field(item, field_schema.arraySubtype) for item in value]
 
         elif isinstance(field_schema, BooleanFieldSchema):
             if not isinstance(value, bool):
-                raise ValueError(f"Cannot parse nonbool type to bool: {field_schema.name}")
+                raise ValueError(
+                    f"Cannot parse nonbool type to bool: {field_schema.name}")
 
             return value
 
         elif isinstance(field_schema, DateFieldSchema):
             if not isinstance(value, str):
-                raise ValueError(f"Cannot parse nonstring date type: {field_schema.name}")
+                raise ValueError(
+                    f"Cannot parse nonstring date type: {field_schema.name}")
 
             if value.endswith("BC"):
-                raise ValueError(f"Cannot parse BC date type: {field_schema.name}")
+                raise ValueError(
+                    f"Cannot parse BC date type: {field_schema.name}")
 
             # Airbyte and foundry are both RFC 3339 date strings
             return value
@@ -253,25 +269,32 @@ class AirbyteToFoundryConverter:
 
         elif isinstance(field_schema, StringFieldSchema):
             if not isinstance(value, str):
-                raise ValueError(f"Cannot parse nonstring string type: {field_schema.name}")
+                raise ValueError(
+                    f"Cannot parse nonstring string type: {field_schema.name}")
 
             return value
 
         elif isinstance(field_schema, StructFieldSchema):
             if not isinstance(value, dict):
-                raise ValueError(f"Cannot parse nonobject struct type: {field_schema.name}")
+                raise ValueError(
+                    f"Cannot parse nonobject struct type: {field_schema.name}")
 
             converted = {}
             for key, value in value.items():
                 sanitized_key_name = sanitize_avro_field_name(key)
-                field_sub_schema = _find_field_schema(sanitized_key_name, field_schema.subSchemas)
-                converted[sanitized_key_name] = AirbyteToFoundryConverter.convert_record_field(value, field_sub_schema)
+                field_sub_schema = _find_field_schema(
+                    sanitized_key_name, field_schema.subSchemas)
+                # TODO(jcrowson): throw an error if not every field is filled
+                if field_sub_schema is not None:
+                    converted[sanitized_key_name] = AirbyteToFoundryConverter.convert_record_field(
+                        value, field_sub_schema)
 
             return converted
 
         elif isinstance(field_schema, TimestampFieldSchema):
             if not isinstance(value, str):
-                raise ValueError(f"Cannot parse nonstring timestamp type: {field_schema.name}")
+                raise ValueError(
+                    f"Cannot parse nonstring timestamp type: {field_schema.name}")
 
             return _convert_timestamp_to_unix_ms(value)
 
@@ -281,7 +304,11 @@ class AirbyteToFoundryConverter:
 
         for key, value in airbyte_record.items():
             sanitized_key = sanitize_avro_field_name(key)
-            field_schema = _find_field_schema(sanitized_key, foundry_schema.fieldSchemaList)
-            converted[sanitized_key] = AirbyteToFoundryConverter.convert_record_field(value, field_schema)
+            field_schema = _find_field_schema(
+                sanitized_key, foundry_schema.fieldSchemaList)
+            # TODO(jcrowson): throw error if not every field is filled
+            if field_schema is not None:
+                converted[sanitized_key] = AirbyteToFoundryConverter.convert_record_field(
+                    value, field_schema)
 
         return converted
--- airbyte-integrations/connectors/destination-palantir-foundry/destination_palantir_foundry/foundry_schema/providers/streams/structured_stream_schema_provider.py
+++ airbyte-integrations/connectors/destination-palantir-foundry/destination_palantir_foundry/foundry_schema/providers/streams/structured_stream_schema_provider.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict, Any
 
 from airbyte_cdk.models.airbyte_protocol import AirbyteStream, AirbyteRecordMessage
@@ -7,6 +8,9 @@ from destination_palantir_foundry.foundry_schema.converters.streams.airbyte_to_s
     convert_ab_to_foundry_stream_schema
 from destination_palantir_foundry.foundry_schema.foundry_schema import FoundrySchema, TimestampFieldSchema
 from destination_palantir_foundry.foundry_schema.providers.streams.stream_schema_provider import StreamSchemaProvider
+
+
+logger = logging.getLogger("airbyte")
 
 
 class StructuredStreamSchemaProvider(StreamSchemaProvider):
@@ -19,7 +23,8 @@ class StructuredStreamSchemaProvider(StreamSchemaProvider):
     """
 
     def get_foundry_stream_schema(self, airbyte_stream: AirbyteStream) -> FoundrySchema:
-        foundry_schema = convert_ab_to_foundry_stream_schema(airbyte_stream.json_schema)
+        foundry_schema = convert_ab_to_foundry_stream_schema(
+            airbyte_stream.json_schema)
 
         metadata_fields = [
             TimestampFieldSchema(
@@ -28,7 +33,8 @@ class StructuredStreamSchemaProvider(StreamSchemaProvider):
             )
         ]
 
-        foundry_schema.fieldSchemaList = [*metadata_fields, *foundry_schema.fieldSchemaList]
+        foundry_schema.fieldSchemaList = [
+            *metadata_fields, *foundry_schema.fieldSchemaList]
 
         return foundry_schema
 
@@ -37,6 +43,7 @@ class StructuredStreamSchemaProvider(StreamSchemaProvider):
             airbyte_record: AirbyteRecordMessage,
             foundry_schema: FoundrySchema,
     ) -> Dict[str, Any]:
+
         return {
             "_ab_emittedAt": airbyte_record.emitted_at,
             **AirbyteToFoundryConverter.convert_ab_record(airbyte_record.data, foundry_schema)
